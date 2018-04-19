ed98b0af35a351f4473458eb76b31a66200b8adf
zhujiangjiang <zhujiangjiang@souche.com>
2018-04-19 10:57:53 +0800

    ddd

--- ArrayList.java
+++ ArrayList.java
@@ -1,4 +1,6 @@
 public class ArrayList<AnyType> implements Iterable<AnyType>{
+    private int d = 100;
+    
     private String haha = "hahha";
 	private int hehe = 50;
 	private static final int DEFAULT_CAPACITY = 10;
